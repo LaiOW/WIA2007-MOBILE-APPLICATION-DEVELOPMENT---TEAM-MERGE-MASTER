e8f7823e4397c6541d7db1a79c57a75d5b3f72de
WeiHong <liewweihong0017@gmail.com>
2026-01-15 23:03:23 +0800

    Enable Back button in category view

--- app/src/main/java/com/example/myapplication/CategoryPostActivity.java
+++ app/src/main/java/com/example/myapplication/CategoryPostActivity.java
@@ -29,4 +29,10 @@ public class CategoryPostActivity extends AppCompatActivity {
         FeedAdapter adapter = new FeedAdapter(postList);
         recyclerCategoryPosts.setAdapter(adapter);
     }
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
 }
